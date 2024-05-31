d70fea9da40befb1032549534c0640f5ceb8d252
alex piku <122716205+Piku090909@users.noreply.github.com>
2024-05-31 09:39:38 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ global.audio= "" ;
 global.video= "" ;
 global.port =process.env.PORT
 global.appUrl=process.env.APP_URL || ""                       // put your app url here,
-global.email ="saimsamsun789@gmail.com"
+global.email ="pikubalur@gmail.com"
 global.location="Lahore,Pakistan."
 
 

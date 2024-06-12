f953f04173e40b84cce250a0fc7417b40627059b
Willis <130226179+WillisKE@users.noreply.github.com>
2024-06-12 18:30:41 +0300

    Update intro.js

--- plugins/intro.js
+++ plugins/intro.js
@@ -16,18 +16,22 @@ var image = 'https://telegra.ph/file/cd250759ecb35a39a9951.jpg'
 
 let text = `╭═══ ━ ━ ━ ━ • ━ ━ ━ ━ ═══♡᭄
 │       〃ᴡɪʟʟɪꜱ〃
-│ Name     : ᴡɪʟʟɪꜱ
-│ Place    : ᴋɪꜱᴜᴍᴜ/ᴋᴇɴʏᴀ
-│ Gender   :  ᴍ
-│ Age      : 18
-│ Phone    : +254786273945
+│ *Name*     : ᴡɪʟʟɪꜱ
+│ *Place*    : ᴋɪꜱᴜᴍᴜ/ᴋᴇɴʏᴀ
+│ *Gender*   :  ᴍ
+│ *Age*      : 18
+│ *Phone*    : +254786273945
 
-│ Support  : M-Pesa BuyGoodsTill - *9657729*
+│ *Support*  : M-Pesa BuyGoodsTill - *9657729*
 
-│ YouTube  : https://www.youtube.com/c/WillisWrld
-│ InstaGram: https://www.instagram.com/Officialwilis
-│ FaceBook : https://www.facebook.com/Officialwilis
-│ GitHub   : https://github.com/WillisKE  
+│ *YouTube*  :↷
+│ https://www.youtube.com/c/WillisWrld
+│ InstaGram:↷
+│ • https://www.instagram.com/Officialwilis
+│ *FaceBook* :↷
+│ • https://www.facebook.com/Officialwilis
+│ *GitHub*   :↷
+│ • https://github.com/WillisKE  
 │ Status   : ᴘʀᴏꜰᴇꜱꜱɪᴏɴᴀʟ ᴅᴇᴠᴇʟᴏᴘᴇʀ 
 ╰═══ ━ ━ ━ ━ • ━ ━ ━ ━ ═══♡᭄`
 
